df04580952f546cf64dfefd1471f6876a10c2b11
Rohit Rawat <rohitrawat4159@gmail.com>
2024-01-04 18:12:13 +0530

    Initial Commit

---
+++ src/main/java/com/speer/src/speerassessmentbackend/SpeerAssessmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.speer.src.speerassessmentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeerAssessmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeerAssessmentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/speer/src/speerassessmentbackend/SpeerAssessmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speer.src.speerassessmentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeerAssessmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
